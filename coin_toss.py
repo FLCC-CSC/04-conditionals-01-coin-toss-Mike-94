62b3827c8c99dc98507c7a0fe94e26cb8c08892c
Michael Reed <mreed45@fingerlakes.edu>
2026-02-23 01:55:22 +0000

    Coin flipper random.randint

--- coin_toss.py
+++ coin_toss.py
@@ -1,21 +1,20 @@
 # FILE NAME - coin_toss.py
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+# NAME: Michael Reed
+# DATE: 2/22/2026
+# BRIEF DESCRIPTION: This program is a simple application that will randomly return Heads or Tails.  
 # 1. Make sure you fill out the comments above
 # 2. Write your code in the proper spot
 # 3. Be sure to answer the Reflection Questions and Attestation below
 # 4. The Sample Output has been included in this code for your convenience
 ########## ENTER YER CODE BELOW THIS LINE ##########
 
-
-
-
-
-
-
-
-
+import random
+print("==== Coin Flipper ====")
+number = random.randint(1, 100)
+if number >= 51:
+    print("Tails")
+else:
+    print("Heads")
 
 ########### END YER CODE ABOVE THIS LINE ###########
 
@@ -43,7 +42,7 @@ Tails
 '''
 
 1. What was the hardest part of completing this lab? 
-
+There was not one specific thing that i thought was hard about this lab.
 
 
 
@@ -62,6 +61,6 @@ like AI and internet sites like Chegg, CourseHero, StackOverflow, and general Go
 may impede your understanding. Please rate how well you understand the concepts in this lab: 
 [ ] I understand very little about this lab.
 [ ] I am about 50/50 on this lab; I get parts of it but not the whole picture.
-[ ] I pretty much get it.
+[X] I pretty much get it.
 [ ] I'm solid. Totally got it.
 '''
